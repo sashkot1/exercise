c4970369f0a02fd9d66fe6c1251f51c3c53e5fb8
sashkot <sashkot100@gmail.com>
2018-03-10 18:08:04 +0200

    Updated by sashkot1!

---
+++ gitnHub/gitnHub/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+namespace Arrays7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long[] array = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(long.Parse)
+                .ToArray();
+
+            long checkNumber = long.Parse(Console.ReadLine());
+            long[] reversedArray = array.Reverse().ToArray();
+
+            int counter = 0;
+            long verifier = 0;
+            for (int i = 0; i < reversedArray.Length; i++)
+            {
+
+                counter++;
+                long curentNumber = reversedArray[i];
+                if (curentNumber == checkNumber)
+                {
+                    verifier++;
+                    break;
+                }
+
+            }
+
+            if (verifier == 0)
+            {
+                Console.WriteLine("No occurrences were found!");
+            }
+            else if (verifier >= 1)
+            {
+                long[] arrayToEvaluate = reversedArray.Skip(counter)
+                .Take(reversedArray.Length).ToArray();
+
+                long result = arrayToEvaluate.Sum();
+                Console.WriteLine(result);
+            }
+
+
+
+
+
+
+        }
+    }
+}
